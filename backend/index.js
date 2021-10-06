df7fefa138e2c4f96e1f97a110ae247ee3d9fdcc
Jean Esteves <jean.esteves2001@gmail.com>
2021-10-06 13:46:51 -0300

    parte do backend com array de user e frontend com login

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 
 const PORT = 5000;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const teste = [];
+const users = [{ id: 1, login: 'admin', password: 'admin', permission: 1 }];
 
-app.post('/teste', (req, res) => {
+app.get('/users', (req, res) => {
+  res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
   const { login, password } = req.body;
-  teste.push({ login, password });
-  res.status(200).json(teste);
+  users.push({ id: users.length + 1, login, password, permission: 0 });
+  res.status(200).json(users);
 });
 
 app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
